3475022355b86698a114dd6bb0032d88fbac5d67
mkorthuis <michael.korthuis@gmail.com>
2018-02-05 16:09:00 -0500

    Made a real little POC to apply a CNN on a set of data!   Of course, a few things are hacked, (like CSS).

--- poc/views.py
+++ poc/views.py
@@ -4,12 +4,15 @@ import os
 import spacy
 from spacy import displacy
 
+from .models import SpacyModel
+
 # Create your views here.
 def index(request):
-    nlp = spacy.load(os.environ['MODEL_LOCATION'] + "anatomy")
-    doc = nlp(u'STUDY:   X-RAY CHEST  REASON FOR EXAM:   Male, 56 years old.  Cough  TECHNIQUE:   PA and lateral views of the chest.  COMPARISON:   None. ___________________________________  FINDINGS:  The lungs are clear and expanded.  Mild blunting of the posterior costophrenic angles.  This is most likely the right costophrenic angle suggesting mild effusion or pleural thickening.  Normal size heart.   Normal mediastinum and hila.  Normal visualized pulmonary arteries.  Normal visualized aortic arch and descending thoracic aorta.  Normal visualized thoracic spine.  Normal visualized ribs, clavicles, and shoulders.  There is no demonstrated abnormality of the visualized soft tissue structures of the upper abdomen. ___________________________________  IMPRESSION:  Mild blunting of one of the posterior costophrenic angles.  This is most likely the right costophrenic angle suggesting mild effusion or pleural thickening.  Lungs are essentially clear.  No prior studies available for my review')
+    modelList = SpacyModel.objects.all();
+    return render(request,'poc/index.html', {'modelList' : modelList})
 
-    options = {'compact': True, 'distance': 105}
+def displayResults(request):
+    nlp = spacy.load(os.environ['MODEL_LOCATION'] + request.POST.get('model',''))
+    doc = nlp(request.POST.get('text', ''))
     html = displacy.render(doc, style="ent")
-
-    return render(request, 'poc/index.html', {'request' : request, 'doc': doc, 'html' : html})
+    return render(request, 'poc/displayResults.html', {'request' : request, 'doc': doc, 'html' : html})
--- whistler/urls.py
+++ whistler/urls.py
@@ -18,6 +18,6 @@ from django.contrib import admin
 from django.urls import include, path
 
 urlpatterns = [
-    path('poc/', include('poc.urls')),
+    path('', include('poc.urls')),
     path('admin/', admin.site.urls),
 ]

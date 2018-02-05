46807a68a6de840acaa39e51918c0866d882f76e
mkorthuis <michael.korthuis@gmail.com>
2018-02-05 14:16:17 -0500

    New folder structure to support more than one model going forward.

--- poc/views.py
+++ poc/views.py
@@ -6,7 +6,7 @@ from spacy import displacy
 
 # Create your views here.
 def index(request):
-    nlp = spacy.load(os.environ['MODEL_LOCATION'])
+    nlp = spacy.load(os.environ['MODEL_LOCATION'] + "anatomy")
     doc = nlp(u'STUDY:   X-RAY CHEST  REASON FOR EXAM:   Male, 56 years old.  Cough  TECHNIQUE:   PA and lateral views of the chest.  COMPARISON:   None. ___________________________________  FINDINGS:  The lungs are clear and expanded.  Mild blunting of the posterior costophrenic angles.  This is most likely the right costophrenic angle suggesting mild effusion or pleural thickening.  Normal size heart.   Normal mediastinum and hila.  Normal visualized pulmonary arteries.  Normal visualized aortic arch and descending thoracic aorta.  Normal visualized thoracic spine.  Normal visualized ribs, clavicles, and shoulders.  There is no demonstrated abnormality of the visualized soft tissue structures of the upper abdomen. ___________________________________  IMPRESSION:  Mild blunting of one of the posterior costophrenic angles.  This is most likely the right costophrenic angle suggesting mild effusion or pleural thickening.  Lungs are essentially clear.  No prior studies available for my review')
 
     options = {'compact': True, 'distance': 105}
